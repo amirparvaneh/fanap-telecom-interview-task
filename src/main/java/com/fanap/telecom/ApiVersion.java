1346662557d7f66393cb08f159f2e9eab400ba66
amirparvaneh <amirparvaneh138@gmail.com>
2023-08-30 21:54:04 +0330

    starting the project configuration

---
+++ src/main/java/com/fanap/telecom/ApiVersion.java
@@ -0,0 +1,5 @@
+package com.fanap.telecom;
+
+public interface ApiVersion {
+    String VERSION_1 = "/api/v1";
+}
---
+++ src/main/java/com/fanap/telecom/service/BaseService.java
@@ -0,0 +1,7 @@
+package com.fanap.telecom.service;
+
+public interface BaseService<T> {
+    void save(T t);
+    void delete(Long id);
+    T find(Long id);
+}
